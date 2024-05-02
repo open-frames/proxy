d80ec571e270adf63bd5058010ea9b1d5295d631
daria-github <daria@xmtp.com>
2024-05-02 09:41:36 -0700

    update OpenFrameButtonResult type

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -34,7 +34,7 @@ export type OpenFrameButtonResult =
 			action: 'link' | 'mint';
 			target: string;
 			label: string;
-			postUrl?: string;
+			postUrl?: never;
 	  }
 	| {
 			action: 'post' | 'post_redirect';
